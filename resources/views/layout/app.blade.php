510ff317b053a1d2ceb7d4b2cc427639b7aacda1
devsabbirhossain <sabbir2dev@gmail.com>
2024-08-24 17:55:39 +0600

    add assect url

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,22 +9,22 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/bootstrap-icons.css" rel="stylesheet">
-        <link href="css/magnific-popup.css" rel="stylesheet">
-        <link href="css/templatemo-first-portfolio-style.css" rel="stylesheet">
+        <link href="{{ asset( 'css/bootstrap.min.css' ) }}" rel="stylesheet">
+        <link href="{{ asset( 'css/bootstrap-icons.css' ) }}" rel="stylesheet">
+        <link href="{{ asset( 'css/magnific-popup.css' ) }}" rel="stylesheet">
+        <link href="{{ asset( 'css/templatemo-first-portfolio-style.css' ) }}" rel="stylesheet">
     </head>
     <body>
         @include( 'component.Header' )
             @yield( 'portfolio' )
         @include( 'component.Footer' )
         <!-- JAVASCRIPT FILES -->
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/jquery.sticky.js"></script>
-        <script src="js/click-scroll.js"></script>
-        <script src="js/jquery.magnific-popup.min.js"></script>
-        <script src="js/magnific-popup-options.js"></script>
-        <script src="js/custom.js"></script>
+        <script src="{{ asset( 'js/jquery.min.js' ) }}"></script>
+        <script src="{{ asset( 'js/bootstrap.min.js' ) }}"></script>
+        <script src="{{ asset( 'js/jquery.sticky.js' ) }}"></script>
+        <script src="{{ asset( 'js/click-scroll.js' ) }}"></script>
+        <script src="{{ asset( 'js/jquery.magnific-popup.min.js' ) }}"></script>
+        <script src="{{ asset( 'js/magnific-popup-options.js' ) }}"></script>
+        <script src="{{ asset( 'js/custom.js' ) }}"></script>
     </body>
 </html>
